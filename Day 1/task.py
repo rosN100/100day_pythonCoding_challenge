776169b64edf0692e3323aa7b96079739a5a286e
Roshan Kumar <roshankumar@Roshans-MacBook-Pro.local>
2025-07-27 21:27:44 +0530

    Initial commit: 100 Days Python Challenge

---
+++ Day 1/task.py
@@ -0,0 +1,41 @@
+"""
+print("Hello World")
+print(f"1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n"
+f"2. Knead the dough for 10 minutes.\n"
+f"3. Add 3g of Salt.\n"
+f"4. Leave to rise for 2 hours.\n"
+f"5. Bake at 200 degrees C for 30 minutes.\n")
+print("Hello world!\nHello world!\nHello world!")
+print("Hello"+" "+"roshan")
+print("Hello "+ "roshan")
+print ("Hello"+" roshan")
+"""
+
+#syntex error
+#indentation error
+
+#Assignment
+"""
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the" + "sign")
+print("New lines can be created with a \n and the letter n")
+"""
+# input("have you used input syntex this way?")
+# print("Hello "+input("what's your name?")+"!")
+# last_name = input("What's your last name?")
+# age = input("what's your age?")
+
+# name = "mohit"
+# print(f"{name} {len(name)}")
+# name ="Roshan"
+# print(f"{name} {len(name)}")
+# num = 234
+# print(len(input("what's your name?")))
+# username = input("what's your name?")
+# length = len(username)
+# print(length)
+
+a = 12
+print(a)
